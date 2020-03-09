dac3b4dc88d7b2888ff40f1e5f86709f8e154287
Ibrahim Coulibaly <casyghuni2010@hotmail.fr>
2020-03-08 23:29:54 -0400

    initial commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+function init(){
+    $("#dateDepart").datepicker({
+        altFormat: "dd-mm-yy"
+      });
+    $("#dateRetour").datepicker({
+        altFormat: "yy-mm-dd"
+      });
+}
+
+var ville = new Array("montreal", "new york");
+
+var vol = new Object();
+
+//constructeur de vol
+function Vol(origine, destination, prix){
+}
+
+function rechercherVolForm(){
+    var txtOrigine = document.getElementById("depart");
+    var origine = txtOrigine.value;
+    var txtDestination = document.getElementById("destination");
+    var destination = txtDestination.value;
+    var txtNbPassagers = document.getElementById("passagers");    
+    var nbPassagers = txtNbPassagers.value;
+    var txt = origine + destination + nbPassagers;
+    alert(txt);
+}
